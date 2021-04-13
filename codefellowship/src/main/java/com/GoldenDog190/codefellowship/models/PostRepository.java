783613e9ded1a3820bbc3a179c77f44c02f1587b
Amelia Waggoner <wildginger@wavecable.com>
2021-04-13 08:26:47 -0700

    added post entity, added classes for userpost, postcontroller, and postrepository

---
+++ codefellowship/src/main/java/com/GoldenDog190/codefellowship/models/PostRepository.java
@@ -0,0 +1,7 @@
+package com.GoldenDog190.codefellowship.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<UserPost, Long> {
+
+}
---
+++ codefellowship/src/main/java/com/GoldenDog190/codefellowship/models/UserPost.java
@@ -0,0 +1,56 @@
+package com.GoldenDog190.codefellowship.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class UserPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @CreationTimestamp
+    String body;
+    LocalDateTime createdAt;
+
+    @ManyToOne
+    ApplicationUser applicationUser;
+
+    public UserPost(String body, LocalDateTime createdAt) {
+        this.body = body;
+        this.createdAt =createdAt;
+    }
+
+    public UserPost(){}
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public UserPost(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
