e7af2883e7e551db070769eb5d0bae25cce84080
Amelia Waggoner <wildginger@wavecable.com>
2021-04-12 17:47:43 -0700

    finished step 6 and 7

--- codefellowship/src/main/java/com/GoldenDog190/codefellowship/controllers/ApplicationController.java
+++ codefellowship/src/main/java/com/GoldenDog190/codefellowship/controllers/ApplicationController.java
@@ -1,15 +1,30 @@
 package com.GoldenDog190.codefellowship.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 
 public class ApplicationController {
 
     @GetMapping("/")
-    public String ShowSplashPage(){
+    public String ShowSplashPage(Principal p){
+        System.out.println("p" + p);
+        if(p != null){
+            System.out.println("p.getName() = " + p.getName());
+        }
         return "splashpage.html";
+    }
+
+    @GetMapping("/applicationUsers")
+    public String showUsers(Principal p, Model m){
+        System.out.println("p.getName() = " + p.getName());
+
+        m.addAttribute("user", p.getName());
+        return "applicationUsers";
     }
 
 }
--- codefellowship/src/main/java/com/GoldenDog190/codefellowship/controllers/ApplicationUserController.java
+++ codefellowship/src/main/java/com/GoldenDog190/codefellowship/controllers/ApplicationUserController.java
@@ -1,17 +1,34 @@
 package com.GoldenDog190.codefellowship.controllers;
 
 
+import com.GoldenDog190.codefellowship.models.ApplicationUser;
+import com.GoldenDog190.codefellowship.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Controller
 public class ApplicationUserController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
 
     @PostMapping("/applicationUser")
     public RedirectView createUser(String username, String password, String firstName, String lastName, int dateOfBirth, String bio){
+        password = passwordEncoder.encode(password);
+        System.out.println("password= " + password);
+        ApplicationUser applicationUser = new ApplicationUser();
+        applicationUser.setPassword(password);
+        applicationUser.setApplicationUser(username);
+        applicationUser.setApplicationUser(firstName);
+        applicationUser.setApplicationUser(lastName);
+        applicationUser.setApplicationUser(String.valueOf(dateOfBirth));
+        applicationUser.setApplicationUser(bio);
         return new RedirectView("/");
     }
 
